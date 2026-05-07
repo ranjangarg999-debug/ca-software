9358c41933cdf4a7c67f395fbcb5a8b3f6a5e6af
ranjangarg999-debug <ranjangarg999@gmail.com>
2026-05-07 18:33:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post("/generate", (req, res) => {
 });
 
 //  last me server start
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
