78c808bf4cec60c0015a20ad4261734e0ac14a6e
ranjangarg999-debug <ranjangarg999@gmail.com>
2026-05-08 16:25:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.post("/generate", (req, res) => {
   res.json({ result });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
